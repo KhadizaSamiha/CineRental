c8e1d9644eab1390b8d867bd83d6740baff24e19
Khadiza Samiha <khadizasamiha7@gmail.com>
2025-05-17 22:33:22 +0600

    initial setup for cine-rental

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-blue-400 text-3xl">Cine Rental</h1>
+      <h1 className="text-blue-400 text-2xl">Cine Rental</h1>
     </>
   );
 }
