691326ae58bec0fffa6333b75ebe4671b84efda2
Khadiza Samiha <khadizasamiha7@gmail.com>
2025-05-18 22:15:48 +0600

    cartDetails functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
-import Logo from "../assets/logo.svg"
-import RingIcon from "../assets/ring.svg"
-import MoonIcon from "../assets/icons/moon.svg"
-import ShoppingCartIcon from "../assets/shoppingCart.svg"
+import Logo from "../assets/logo.svg";
+import RingIcon from "../assets/ring.svg";
+import MoonIcon from "../assets/icons/moon.svg";
+import ShoppingCartIcon from "../assets/shoppingCart.svg";
+import { useState } from "react";
+import CartDetails from '../components/cine/CartDetails'
 
 const Header = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  const handleCartShow = () =>{
+    setShowCart(!showCart)
+  }
   return (
     <header>
+      {showCart && <CartDetails onClose={() => setShowCart(false)}/>}
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={Logo} width="139" height="26" alt="" />
@@ -30,15 +38,11 @@ const Header = () => {
           </li>
           <li>
             <a
+             onClick={handleCartShow}
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
-              <img
-                src={ShoppingCartIcon}
-                width="24"
-                height="24"
-                alt=""
-              />
+              <img src={ShoppingCartIcon} width="24" height="24" alt="" />
             </a>
           </li>
         </ul>
