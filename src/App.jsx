9579a1a61cf4ae5a6953575371505936d0577300
Khadiza Samiha <khadizasamiha7@gmail.com>
2025-05-18 21:07:51 +0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import MovieList from "./components/cine/MovieList";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
@@ -15,6 +16,8 @@ function App() {
             <MovieList/>
           </div>
         </main>
+
+        <Footer />
       </div>
     </>
   );
