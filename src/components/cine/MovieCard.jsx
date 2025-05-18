e89a910b94bd026c4249ce64592653472e319e41
Khadiza Samiha <khadizasamiha7@gmail.com>
2025-05-18 21:05:02 +0600

    rating

--- src/components/cine/MovieCard.jsx
+++ src/components/cine/MovieCard.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { getImgUrl } from "../../utils/cine-utils";
+import Rating from "./Rating";
 
 const MovieCard = ({ movie }) => {
   return (
@@ -9,11 +9,7 @@ const MovieCard = ({ movie }) => {
         <h3 className="text-xl mb-1">{movie.title}</h3>
         <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
         <div className="flex items-center space-x-1 mb-5">
-          {[...Array(movie.rating)].map((_, i) => (
-            <img key={i} src={getImgUrl("star.svg")} width="14" height="14" alt="" />
-          ))}
-          <img src={getImgUrl("star.svg")} width="14" height="14" alt="" />
-          <img src={getImgUrl("star.svg")} width="14" height="14" alt="" />
+         <Rating value={movie.rating} />
         </div>
         <a
           className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
